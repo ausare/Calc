54f5236fbf114545239547a690e13e8b787633ba
Jason <ausare@msn.com>
2018-09-15 09:52:27 -0400

    This is my first comment

---
+++ calc.js
@@ -0,0 +1,10 @@
+/*
+ * Implement all your JavaScript in this file!
+ */
+$("#id1").text("This is how we do it!");
+ $("#button1").click(function(){
+     $('#display').val("1");
+ });
+ $("#button2").click(function(){
+    $('#display').val("2");
+ });
